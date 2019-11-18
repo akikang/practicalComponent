2069e92f96d45f525cc2d182dbf0215c485561d7
cheng hang <akikang666@gmail.com>
2019-11-18 23:44:28 +0900

    get the device type

--- 001-010/001-010.js
+++ 001-010/001-010.js
@@ -108,3 +108,16 @@ function batch_delete() {
 }
 
 // ********************04********************
+function judgeClient() {
+  let u = navigator.userAgent;
+  let isAndroid = u.indexOf("Android") > -1 || u.indexOf("Adr") > -1; //判断是否是 android终端
+  let isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //判断是否是 iOS终端
+  if (isAndroid) {
+    alert("Android");
+  } else if (isIOS) {
+    alert("iOS");
+  } else {
+    alert("else");
+  }
+}
+// ********************04********************
